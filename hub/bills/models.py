aede6a35d94f3c6badf3322e9b748310e1b90630
Alex Morega <alex@grep.ro>
2015-03-29 15:55:15 +0300

    unicode representation of objects

--- hub/bills/models.py
+++ hub/bills/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Institution(models.Model):
     name = models.CharField(max_length=1024)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Bill(models.Model):
     key = models.CharField(max_length=1024)
     title = models.CharField(max_length=1024)
     url = models.URLField(max_length=1024)
     institution = models.ForeignKey(Institution)
+
+    def __unicode__(self):
+        return u"{s.title} ({s.institution.name})".format(s=self)
