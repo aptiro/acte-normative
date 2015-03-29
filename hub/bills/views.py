7e56e0ed761de686e48a2e4dada9cd15cf6be075
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-03-29 17:04:18 +0300

    Move comment to the right place #1

--- hub/bills/views.py
+++ hub/bills/views.py
@@ -11,8 +11,8 @@ from models import Bill, Institution
 def receive_bills(request):
     received_bills = json.loads(request.body)
     for received_bill in received_bills:
-        # TODO: use authorization by API key
         title = received_bill['title']
+        # TODO: use authorization by API key
         institution = Institution.objects.first()
         bill = Bill(title=title,
                     url=received_bill['url'],
