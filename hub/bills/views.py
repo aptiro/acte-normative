89e66a5053e93dbfb487d211e53ca05713727ac6
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-03-29 16:48:33 +0300

    Add default institution #1

--- hub/bills/views.py
+++ hub/bills/views.py
@@ -5,14 +5,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from models import Bill
+from models import Bill, Institution
 
 @csrf_exempt
 def receive_bills(request):
     received_bills = json.loads(request.body)
     for received_bill in received_bills:
+        # TODO: use authorization by API key
+        institution = Institution.objects.first()
         bill = Bill(title=received_bill['title'],
                     url=received_bill['url'],
-                    key=hashlib.sha1(received_bill['title'].encode('utf-8')))
+                    key=hashlib.sha1(received_bill['title'].encode('utf-8')),
+                    institution=institution)
         bill.save()
     return JsonResponse({'status': 'ok'})
